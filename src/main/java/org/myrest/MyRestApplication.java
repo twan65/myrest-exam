635f2680c96815e912d78496032db2864400f976
twan65 <cx.twan65@gmail.com>
2021-12-06 21:13:45 +0900

    Initial commit

---
+++ src/main/java/org/myrest/MyRestApplication.java
@@ -0,0 +1,12 @@
+package org.myrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyRestApplication.class, args);
+  }
+}
